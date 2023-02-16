71d402ac262bec09675b59b036285de14baa18d2
JesseTheRobot <jesse.cruz.wright@gmail.com>
2023-02-16 10:42:34 +0000

    feat: :sparkles: Add Proof-ID tag

--- helium-lambda/app.ts
+++ helium-lambda/app.ts
@@ -1,13 +1,15 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import Bundlr from '@bundlr-network/client';
-
+import Crypto from 'crypto';
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     let response: APIGatewayProxyResult;
     try {
         // BUNDLR_URL needs to be in the fully qualified form, e.g`https://node1.bundlr.network`.
         // SOLANA_KEY needs to be the full 88 character long private key string, e.g `9aV2tL9hHM6hyr3QHPfgm5SbYqt4XqNMLE9AtXqAsbcTE1t9ic7vRtf69Ne9A6x7dGcYTa5W8yUysweTEpLgC7mv`
         const bundlr = new Bundlr(process.env.BUNDLR_URL!, 'solana', process.env.SOLANA_KEY!);
-        const res = await bundlr.upload(event.body);
+        const res = await bundlr.upload(event.body!, {
+            tags: [{ name: 'Proof-ID', value: Crypto.randomBytes(32).toString('hex') }],
+        });
         const transactionId = res.id; // ID of the upload, accessible via `https://arweave.net/${transactionId}`
         return {
             statusCode: 200,
